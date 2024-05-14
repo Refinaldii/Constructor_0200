ba7cf1805206bd82f5d1f3eebe0b836abdbee925
Refinaldii <refinaldi9786@gmail.com>
2024-05-14 11:05:54 +0700

    membuat lingkup private dan public dari class mahasiswa

--- Construct/Construct.cpp
+++ Construct/Construct.cpp
@@ -1,7 +1,19 @@
-
-
 #include <iostream>
+#include <string>
+using namespace std;
 
+class Mahasiswa {
+private :
+    int nim;
+    string nama;
+public:
+    Mahasiswa();
+    Mahasiswa(int);
+    Mahasiswa(string);
+    Mahasiswa(int iNim, string iNama);
+    void cetak();
+
+};
 int main()
 {
     std::cout << "Hello World!\n";
