0f98354a479a321fa97376c4802ba811b68a486f
Refinaldii <refinaldi9786@gmail.com>
2024-05-14 10:39:00 +0700

    membuat isi dari int main

--- ConstructorBaru.cpp
+++ ConstructorBaru.cpp
@@ -33,6 +33,18 @@ void Mahasiswa::printAll() {
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    system("pause");
+
+    return 0;
 }
 
