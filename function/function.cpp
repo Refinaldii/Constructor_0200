2b6f69b4dce17e55541372cf87cfb6d1fdf410e9
Refinaldii <refinaldi9786@gmail.com>
2024-05-14 10:49:48 +0700

    membuat void mahasiswa untuk set id

--- function/function.cpp
+++ function/function.cpp
@@ -14,8 +14,15 @@ public:
 
     static void setNim(int pNim) { nim = pNim; }
     static int getNIm() { return nim;  }
+
+    Mahasiswa(string pnama) :nama(pnama) { setID(); }
        
 };
+
+int Mahasiswa::nim = 0;
+void Mahasiswa::setID() {
+    id = ++nim;
+}
 int main()
 {
     std::cout << "Hello World!\n";
