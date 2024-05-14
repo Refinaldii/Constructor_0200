e5df8eb94b969e956dcd4b199eed4ef8d7c50e1e
Refinaldii <refinaldi9786@gmail.com>
2024-05-14 12:46:20 +0700

    membuat constructor dan destructor

--- deskruktor/deskruktor.cpp
+++ deskruktor/deskruktor.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+class angka {
+private:
+    int* arr;
+    int panjang;
+public:
+    angka(int);
+    ~angka();
+    void cetakData();
+    void isiData();
+};
+
+angka::angka(int i) {
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka() {
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+
 
 int main()
 {
